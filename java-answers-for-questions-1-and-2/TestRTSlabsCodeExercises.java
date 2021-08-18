7a39c07e4cf0af9ba6934b2d147a6330565eca95
john million <jpmillion@comcast.net>
2021-08-18 18:46:38 -0400

    Delete instance of RTSlabsCodeExercises

--- java-answers-for-questions-1-and-2/TestRTSlabsCodeExercises.java
+++ java-answers-for-questions-1-and-2/TestRTSlabsCodeExercises.java
@@ -5,21 +5,21 @@ import jdk.jfr.Timestamp;
 import static org.junit.Assert.assertEquals;
 
 public class TestRTSlabsCodeExercises {
-    RTSlabsCodeExercises codeExercises = new RTSlabsCodeExercises();
+    
 
     @Test
     public void testElementsAboveAndBelowN() {
 
         int[] arr = {46,9,-23,0,-45,88,12};
 
-        String string = codeExercises.elementsAboveAndBelowN(arr, 0);
+        String string = RTSlabsCodeExercises.elementsAboveAndBelowN(arr, 0);
         assertEquals("above: 4, below: 2", string);
     }
 
     @Test 
     public void testRotateByN() {
 
-        String string = codeExercises.rotateByN("Pittsburgh Steelers", 3);
+        String string = RTSlabsCodeExercises.rotateByN("Pittsburgh Steelers", 3);
         assertEquals("ersPittsburgh Steel", string);
     }
 }
